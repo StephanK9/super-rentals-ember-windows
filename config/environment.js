3305addff91a9638fbfb882dea499ec527d40658
StephanK9 <stephane.khoury@gmail.com>
2017-01-11 11:26:51 -0800

    fresh commit of super-rentals from a new computer

---
+++ config/environment.js
@@ -0,0 +1,60 @@
+/* jshint node: true */
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'super-rentals',
+    environment: environment,
+    rootURL: '/',
+    locationType: 'auto',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      },
+
+
+
+
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
+      }
+    },
+    firebase: {
+     apiKey: 'AIzaSyDJ00Lnq2Cvkc-aBg6XownsEJed_31jIws',
+     authDomain: 'super-rentals-e06b1.firebaseapp.com',
+     databaseURL: 'https://super-rentals-e06b1.firebaseio.com',
+     storageBucket: 'super-rentals-e06b1.appspot.com'
+   },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    }
+  };
+
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.locationType = 'none';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+
+  }
+
+  return ENV;
+};
