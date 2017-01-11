2e5de902f28d6eb3d1eb06acfc16557b643504f8
StephanK9 <stephane.khoury@gmail.com>
2017-01-11 12:47:57 -0800

    add review model, ability to create a new review and publish it to the index page

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -1,21 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(params) {
-    return this.store.findRecord('rental', params.rental_id);
+  model() {
+    return Ember.RSVP.hash({
+      rentals: this.store.findAll('rental'),
+      reviews: this.store.findAll('review')
+    });
   },
   actions: {
-    update(rental, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
-          rental.set(key,params[key]);
-        }
-      });
-      rental.save();
+    saveRental3(params) {
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
       this.transitionTo('index');
     },
-    destroyRental(rental) {
-      rental.destroyRecord();
+    saveReview(params) {
+      var newReview = this.store.createRecord('review', params);
+      newReview.save();
       this.transitionTo('index');
     }
   }
